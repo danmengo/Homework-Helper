f690ab27f305ca05eb3916799c01d36284a1270c
Jason Wang <jasonwang@dhcp-v064-139.mobile.uci.edu>
2024-01-28 08:54:45 -0800

    universal filepath

--- public/jasonshwproj/equipscan.py
+++ public/jasonshwproj/equipscan.py
@@ -1,7 +1,7 @@
 equip = ["balance", "flasks", "icebath", "balancebrush", "büchnerfunnel" "beaker", "beakers", "bentadaptor", "meltingrange", "vial", "boilingchips", "boilingsticks", "buchnerfunnel", "columnforchromatography", "condenser", "conicalcentrifugetube", "cotton", "disposablepipette", "digimelt", "distillationhead", "distillationsetup", "erlenmeyerflask", "filterflask", "filterflaskadapter", "filterfunnel", "filterpaperforbuchnerfunnel", "filterpaperforhirschfunnel", "forcep", "ftir", "fritforhirschfunnel", "gcvial", "glassthermometer", "glassfunnel", "glasspipette", "glasswastecontainer", "graduatedcylinder", "grease", "hirschfunnel", "heatingblock", "hotplate", "icebucket", "keckclip", "kimwipe", "labelingtape", "labjack", "liquid/solidwastecontainer", "magneticwand", "meltingpointcapillarytubes", "mct(microcentrifugetube)", "mctrack", "micropipette", "micropipettetips", "microscaleaircondenser", "microscalereactiontube", "mortar", "nmrtube", "parafilm", "pestle", "phpaper", "plasticfunnel", "plasticpipette", "potassiumiodide-starchpaper", "ringclamp", "roundbottomflask", "roundbottomedflask", "rubberpipettebulb", "scintillationvial", "separatoryfunnel", "secondarycontainer", "septa", "scoop", "stirbar", "stirringrod", "tubing", "testtube", "testtubeclamp", "testtuberack", "thermocouple", "thermometeradaptor", "tlcjar", "tlcplate", "tlcspotter", "tlcvisualizingchamber", "uvlamp", "uv", "washbottle", "wastelabel", "waterlesscondenser", "watchglass", "weighpaper"]
 inventory = []
 filename = input("enter file name: ")
-with open(f'/Users/jasonwang/Downloads/{filename}') as f:
+with open({filename}) as f:
     x = f.readlines()
     for line in x:
         processed = ''.join(line.split())
